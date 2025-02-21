b8cc012f20b55fc8e44b4e4df2d5bbf0d73c3c06
Heike-Petzel <heike.petzel@de.ibm.com>
2025-02-21 13:47:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['JavaScript', 'HTML', 'CSS'],
+        salary: 1000
+    }
+    return member;
+}
